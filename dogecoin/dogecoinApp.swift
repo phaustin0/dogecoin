f154bfcf654a0de250150e1b9eb019047862d5a9
phaustin0 <zechy14@icloud.com>
2021-09-24 18:20:33 +0800

    Initial Commit

---
+++ dogecoin/dogecoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  dogecoinApp.swift
+//  dogecoin
+//
+//  Created by Zechariah Singh on 24/9/21.
+//
+
+import SwiftUI
+
+@main
+struct dogecoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
